5820544f21757948372b430da6567abb1b2905e6
Ilya Chistousov <kw4s1k2@gmail.com>
2022-06-26 14:33:47 +0500

    Init project

---
+++ app/src/main/java/ilya/chistousov/countcalories/MainActivity.kt
@@ -0,0 +1,11 @@
+package ilya.chistousov.countcalories
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
